d4e91ab7dec284516258ba8948e6c53c1e25f375
Elisey <zhdanov.ea@phystech.edu>
2023-04-06 10:16:10 +0300

    41

---
+++ 4_lesson/task1.py
@@ -0,0 +1,42 @@
+import RPi.GPIO as gpio
+from time import sleep, clock
+
+dac=[26, 19, 13, 6, 5, 11, 9, 10]
+bits = len(dac)
+levels = 2 ** bits
+maxVoltage = 3.3
+troykaModule = 17
+comparator = 4
+
+gpio.setmode(gpio.BCM)
+gpio.setup(dac, gpio.OUT, inilial=gpio.LOW)
+gpio.setup(troykaModule, gpio.OUT, initial=gpio.HIGH)
+gpio.setup(comparator, gpio.IN)
+
+def decimal2binary(decimal):
+    return [int (elem) for elem in bin(decimal)[2:].zfill(bits)]
+
+def num2dac(value):
+    signal = decimal2binary(value)
+    gpio.output(dac, signal)
+    return signal
+
+def adc():
+    for value in range(256):
+        signal = num2dac(value)
+        sleep(0.001)
+        voltage = value / levels * maxVoltage
+        comparatorvalue = gpio.input(comparator)
+        if comparatorvalue == 1:
+            print("ADC value = {:^3} -> {}, input voltage = {:2f}".format(value, signal, voltage))
+
+try:
+    while (True):
+        adc()
+except KeyboardInterrupt:
+    print("Stopped by user")
+else:
+    print("NOEXCEPTIONS")
+finally:
+    gpio.output(dac, gpio.LOW)
+    gpio.cleanup()
